6a11874afb359b73ae9ca2a63de3dc78058d2b26
Patel Aryankumar <aryanpatel19082003@gmail.com>
2025-09-28 09:37:57 +0530

    Flexibility of slot and flags

---
+++ 14-flexibility-of-slot-&-flags/js/components/AssignmentList.js
@@ -0,0 +1,47 @@
+import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
+
+export default{
+    components:{ Assignment, AssignmentTags },
+    template: `
+        <section v-show="assignments.length"  class="w-60">
+            <div class="flex justify-between item-start">
+                <h2 class="font-bold mb-2">{{ title }} ({{ assignments.length }})</h2>
+                <button v-show="canToggle" @click="$emit('toggle')"> &times; </button>
+            </div>
+            <assignment-tags 
+                :initial-tags="assignments.map(a => a.tag)"
+                v-model:currentTag = "currentTag"
+                />
+
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-4">
+                <assignment
+                    v-for="assignment in filteredAssignment"
+                    :key="assignment.id"
+                    :assignment="assignment" 
+                />
+            </ul>
+            <slot></slot>
+        </section>
+    `,
+    props:{
+        assignments: Array,
+        title: String,
+        canToggle: { type: Boolean, default: false }
+    },
+    data(){
+        return {
+            currentTag: 'all'
+        };
+    },
+    computed:{
+        filteredAssignment(){
+            if(this.currentTag == 'all'){
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag == this.currentTag);
+        },
+       
+    }
+
+}
---
+++ 14-flexibility-of-slot-&-flags/js/components/Assignments.js
@@ -0,0 +1,56 @@
+import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
+
+export default{
+    components: { AssignmentList, AssignmentCreate },
+    template: `
+        <section class="flex gap-8">
+            <assignment-list :assignments="inProgressAssignments" title="In Progress">
+                <assignment-create @add="add"></assignment-create>
+            </assignment-list>
+            <div v-show="showCompleted">
+                <assignment-list 
+                    :assignments="completedAssignments" 
+                    title="Completed" 
+                    canToggle
+                    @toggle="showCompleted = !showCompleted"
+                ></assignment-list> 
+            </div>
+            
+        </section>
+    `,
+    data() {
+        return {
+            assignments: [], 
+            showCompleted: true
+        }
+    },
+    
+    computed:{
+        inProgressAssignments(){
+            return this.assignments.filter(assignment => ! assignment.complete);
+        },
+
+        completedAssignments(){
+            return this.assignments.filter(assignment => assignment.complete);
+        }
+    },
+    
+    created(){
+        fetch('http://localhost:3000/assignments')
+            .then(response => response.json())
+            .then(assignments => {
+                this.assignments = assignments
+            });    
+    },
+    
+    methods:{
+        add(name){
+            this.assignments.push({
+                id: this.assignments.length + 1,
+                name: name,
+                complete: false,
+            })
+        }
+    }
+}
